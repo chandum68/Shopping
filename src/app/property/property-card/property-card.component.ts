a7a8db08759874c9ef8271d0ded117eb758c5677
chandum68 <Global@del1-lhp-n77179.synapse.com>
2023-03-07 16:48:13 +0530

    nav and property card

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+property={
+  id:1,
+  name:"Birla House",
+  type:"House",
+  price:22000
+}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
